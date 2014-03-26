43534f3e50af49045977e4ccd20bec0f50bdbdcb
1vladal1 <1vladal1@gmail.com>
2014-03-26 15:31:53 +0200

    JAVA Samples

---
+++ src/concurrency/CountDownLatchDemo.java
@@ -0,0 +1,69 @@
+package concurrency;
+
+/**
+ * Created by vlad on 26.03.14.
+ */
+
+import java.util.concurrent.*;
+import java.util.*;
+
+class TaskPortion implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private static Random rand = new Random(47);
+    private final CountDownLatch latch;
+
+    TaskPortion(CountDownLatch latch) {
+        this.latch = latch;
+    }
+    public void run() {
+        try {
+            doWork();
+            latch.countDown();
+        } catch (InterruptedException e) {
+            System.out.println(this + " interrupted");
+        }
+    }
+    public void doWork() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(rand.nextInt(2000));
+        System.out.println(this + " ending");
+    }
+    public String toString() {
+        return String.format("%1$-3d ", id);
+    }
+}
+
+class WaitingTask implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private final CountDownLatch latch;
+    WaitingTask(CountDownLatch latch) {
+        this.latch = latch;
+    }
+    public void run() {
+        try {
+            latch.await();
+            System.out.println("Barier goes for " + this);
+        } catch (InterruptedException e) {
+            System.out.println(this + " interrupted");
+        }
+    }
+    public String toString() {
+        return String.format("WaitingTask %1$-3d", id);
+    }
+}
+
+public class CountDownLatchDemo {
+    static final int SIZE = 100;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        CountDownLatch latch = new CountDownLatch(SIZE);
+        for (int i = 0; i < 10; i++)
+            exec.execute(new WaitingTask(latch));
+        for (int i = 0; i < SIZE; i++)
+            exec.execute(new TaskPortion(latch));
+        System.out.println("All tasks started");
+        exec.shutdown();
+    }
+}
