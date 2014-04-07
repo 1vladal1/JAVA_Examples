cc30af725c71a0cb4d226b75deafa8a516d10472
1vladal1 <1vladal1@gmail.com>
2014-04-07 13:20:01 +0300

    JAVA Samples

--- src/io/Alien.java
+++ src/io/Alien.java
@@ -7,4 +7,11 @@ package io;
 import java.io.*;
 //Serializable class
 public class Alien implements Serializable {
+    private static int value = 1;
+    private int method1() {
+        return value;
+    }
+    public int method2() {
+        return method1();
+    }
 }
--- src/io/xfiles/ThawAlien.java
+++ src/io/xfiles/ThawAlien.java
@@ -1,6 +1,8 @@
 package io.xfiles;
 
 import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * Created by vlad on 24.03.14.
@@ -11,5 +13,22 @@ public class ThawAlien {
         Object mystery = in.readObject();
         in.close();
         System.out.println(mystery.getClass());
+
+        //Reflection
+        Method[] methods = mystery.getClass().getDeclaredMethods();
+        Field[] fields = mystery.getClass().getDeclaredFields();
+        System.out.println("\nMethods: ");
+        for (Method method : methods) {
+            System.out.println(method);
+        }
+        System.out.println("\nFields: ");
+        for (Field field : fields) {
+            System.out.println(field);
+        }
+        System.out.println();
+        Method m = mystery.getClass().getDeclaredMethod("method2");
+        m.setAccessible(true);
+        System.out.println("METHOD: " + m + " -> " + m.isAccessible());
+        System.out.println("Running METHOD. Result: " + m.invoke(mystery));
     }
 }
